9c4a2f6ecde6774a215a67ebaf38999af2c70e97
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-07 22:32:03 +0300

    I'm working

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,27 +1,29 @@
 import PropTypes from 'prop-types';
+import '../profile/Profile.css';
 
 const Profile = ({ username, tag, location, avatar, stats }) => {
-  return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" class="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+    let {followers, views, likes} = stats;
+    return (
+    <div className='profile'>
+      <div className='description'>
+        <img src={avatar} alt="User avatar" className='avatar' />
+        <p className='name'>{username}</p>
+        <p className='tag'>@{tag}</p>
+        <p className='location'>{location}</p>
       </div>
 
-      <ul className="stats">
+      <ul className='stats'>
         <li>
-          <span className="label">Followers </span>
-          <span className="quantity">{stats.followers}</span>
+          <span className='label'>Followers </span>
+          <span className='quantity'>{followers}</span>
         </li>
         <li>
-          <span className="label">Views </span>
-          <span className="quantity">{stats.views}</span>
+          <span className='label'>Views </span>
+          <span className='quantity'>{views}</span>
         </li>
         <li>
-          <span className="label">Likes </span>
-          <span className="quantity">{stats.likes}</span>
+          <span className='label'>Likes </span>
+          <span className='quantity'>{likes}</span>
         </li>
       </ul>
     </div>
@@ -37,6 +39,6 @@ Profile.PropTypes = {
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    stats: PropTypes.object.isRequired,
+    stats: PropTypes.object,
 };*/
 
