4833738460c54a208f59124838762b4a51eec363
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-08 14:15:21 +0300

    offline online

--- src/components/friends/FriendsItem.jsx
+++ src/components/friends/FriendsItem.jsx
@@ -1,10 +1,7 @@
 const FriendsItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className="item">
-          { ({isOnline}===true) ?
-              (<span className="status online">On -l.</span>) :
-              (<span className="status">Off-l.</span>)
-          }
+      <li className="item">
+      <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
     </li>
@@ -12,19 +9,3 @@ const FriendsItem = ({ avatar, name, isOnline }) => {
 };
 
 export default FriendsItem;
-
-/*<span className="status">{isOnline}</span> */
-/*
- {unreadMessages.length > 0 ? (
-        <p>You have {unreadMessages.length} unread messages.</p>
-      ) : (
-        <p>No unread messages.</p>
-      )}
-*/
-/*
-<li class="item">
-  <span class="status"></span>
-  <img class="avatar" src="" alt="User avatar" width="48" />
-  <p class="name"></p>
-</li>
-*/
